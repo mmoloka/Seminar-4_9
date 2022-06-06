36815b35501c6cddc7ca16fcacc831f45ea2fad0
«Mikhail» <kopfgolova@gmail.com>
2022-06-06 20:26:56 +0300

    add Task 1

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 63: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+*/
+
+void Natural (int num, int first)
+{
+    System.Console.Write($"{first++}, ");
+    if (first <= num)
+        Natural(num, first);
+}
+
+int number = int.Parse(System.Console.ReadLine());
+int first = 1;
+string line = string.Empty;
+Natural(number,first);
+
+/*for (int i = 1; i <= number; i++)
+{
+    System.Console.Write($"{i}, ");
+}
+
+*/
